4a86cf39fde86f09085de58f7935a10cff9da5d8
Stefan Rauchegger <s.rauchegger@gmx.at>
2011-09-29 14:43:45 +0000

    rest of halma

--- Halma/Scripts/Halma.js
+++ Halma/Scripts/Halma.js
@@ -1,10 +1,19 @@
+/*
+TODO:
+styling
+display move count
+moving
+check for end
+reset
+
+*/
 var Cell = function(row, column){
     var that = this;
     this.row = row;
     this.column = column;
-    this.moveTo = function (x, y){
-        that.row += x;
-        that.column += y;
+    this.moveTo = function (cell){
+        that.row = cell.row;
+        that.column = cell.column;
         };
     }
     
@@ -25,31 +34,54 @@ this.canvasHeight = function(){ return 1 + (that.boardHeight*that.pieceHeight);}
  //       return new Cell(row, column);
   //  };
 
-this.nothingSelected = 1;
+this.nothingSelected = -1;
 
 this.saveGame = function(){
-    
+    if (window.localStorage) {
+        state = JSON.stringify(that.pieces);
+        localStorage['halma.pieces'] = state;
+        localStorage['halma.moveCount'] = that.moveCount; 
+    }
     }
 this.resumeGame = function(){
-    if (window.localStorage) {
-        
+    if (window.localStorage && localStorage['halma.pieces']) {
+        that.pieces = JSON.parse(localStorage['halma.pieces']);
+        // create for each piece a new instance of the piece to get the moveTo handler back, because this one was not persisted
+        for(i = 0; i < that.pieces.length; i++){
+            that.pieces[i] = new Cell(that.pieces[i].row, that.pieces[i].column);
+            }
+        that.moveCount = localStorage['halma.moveCount'];
+        return true;
     }
     return false;
     }    
 
+this.isGameOver = function(){
+    var i, piece;
+    for (var i = 0; i < that.pieces.length; i++) {
+        piece = that.pieces[i];
+        if (piece.row > 2 || piece.col < that.boardWidth - 3) {
+            return false;
+        }
+    }
+    return true;
+    };
+
+
 this.newGame = function(){
     
-    that.pieces = [ new Cell(that.boardHeight - 3, 0),
-                    new Cell(that.boardHeight - 2, 0),
-                    new Cell(that.boardHeight - 1, 0),
-                    new Cell(that.boardHeight - 3, 1),
-                    new Cell(that.boardHeight - 2, 1),
-                    new Cell(that.boardHeight - 1, 1),
-                    new Cell(that.boardHeight - 3, 2),
-                    new Cell(that.boardHeight - 2, 2),
-                    new Cell(that.boardHeight - 1, 2)
-    ];
-    
+    //if (!that.resumeGame()){
+        that.pieces = [ new Cell(that.boardHeight - 3, 0),
+                        new Cell(that.boardHeight - 2, 0),
+                        new Cell(that.boardHeight - 1, 0),
+                        new Cell(that.boardHeight - 3, 1),
+                        new Cell(that.boardHeight - 2, 1),
+                        new Cell(that.boardHeight - 1, 1),
+                        new Cell(that.boardHeight - 3, 2),
+                        new Cell(that.boardHeight - 2, 2),
+                        new Cell(that.boardHeight - 1, 2)
+        ];
+    //}
     that.selectedPieceIndex = that.nothingSelected;
     that.moveCount = 0;
     that.drawBoard();
@@ -60,6 +92,8 @@ this.drawBoard = function(){
     var x, y;
     
     dc.clearRect(0, 0, that.canvasWidth(), that.canvasHeight());
+
+    dc.beginPath();
     
     for(x = 0; x < that.canvasWidth(); x+=that.pieceWidth){
         dc.moveTo(0.5 + x, 0);
@@ -81,7 +115,7 @@ this.drawBoard = function(){
     /*$(this.pieces).each(function(){
             this.drawPiece(this);
         });*/
-        
+    //that.saveGame();
     };
 
 this.drawPiece = function (p, selected){
@@ -144,15 +178,19 @@ this.canvasClick = function(event){
     };
     
 this.pieceClicked = function (index){
-    that.pieces[index].selected = true;
+    if( index === that.selectedPieceIndex ) { return; }
+    //that.pieces[index].selected = true;
     //Halma.drawPiece(Halma.pieces[index]);
     
+     
     that.selectedPieceIndex = index;
+  
+    
     that.drawBoard();
-    alert('piece');
+    
     }; 
 this.emptyCellClicked = function (cell){
-    alert('empty');
+    console.log('empty');
     // if selected piece, then move to adjacent piece
     
     
@@ -164,17 +202,20 @@ this.emptyCellClicked = function (cell){
     }
     
     selectedPiece = that.pieces[that.selectedPieceIndex];
+    
     dx = Math.abs(cell.row - selectedPiece.row);
     dy = Math.abs(cell.column - selectedPiece.column);
     
     if (that.isSimpleMove(dx, dy)) {
-        that.selectedPiece.moveTo(cell);
+        console.log('simple');
+        selectedPiece.moveTo(cell);
         that.moveCount += 1;
         that.selectedPieceIndex = that.nothingSelected;
         that.drawBoard();
     }
-    else if (that.isJump(dx, dy) && that.isPieceInBetween(selectedPiece, cell)) {
-        that.selectedPiece.moveTo(cell);
+    else if (that.isJump(dx, dy) && that.isPieceInbetween(selectedPiece, cell)) {
+        console.log('jump');
+        selectedPiece.moveTo(cell);
         that.moveCount += 1;
         that.selectedPieceIndex = that.nothingSelected;
         that.drawBoard();
@@ -203,22 +244,30 @@ this.getCursorPosition = function (event){
     x = Math.min(x, that.canvasWidth() -1);
     y = Math.min(y, that.canvasHeight() -1);
     
-    cell = new Cell(Math.floor(y / Halma.pieceHeight), 
-                         Math.floor(x / Halma.pieceWidth));
+    cell = new Cell(Math.floor(y / that.pieceHeight), 
+                         Math.floor(x / that.pieceWidth));
     
     return cell;
     };
     
-this.isSimpleMove = function(){
-        
+this.isSimpleMove = function(dx, dy){
+         return (dx <= 1) && (dy <= 1);
     }    
 this.isJump = function (dx, dy){
     return ((dx == 2) && (dy == 0))
-     || ((dx == 2) && (dy == 0))
-     || ((dx == 2) && (dy == 0));
+     || ((dx == 0) && (dy == 2))
+     || ((dx == 2) && (dy == 2));
     }
 this.isPieceInbetween = function (p1, p2){
-    
+    var centreRow = (p1.row + p2.row) / 2;
+  var centreCol = (p1.column + p2.column) / 2;
+  var i;
+  for( i = 0; i < that.pieces.length; i++ ) {
+    if( that.pieces[i].row == centreRow && that.pieces[i].column == centreCol ) {
+      return true;
+    }
+  }
+  return false;
     }    
 };
 
